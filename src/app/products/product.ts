fc8492359dac58d486d8fb25239f6ee6bebfbbfc
Archana Nagrulkar <nagrulkar.archana@gmail.com>
2020-12-30 14:43:27 +0530

    Add files via upload

---
+++ src/app/products/product.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  productId: number;
+  title: string;
+  subtitle: string;
+  author: string;
+  published:string;
+  publisher:string;
+  price: number;
+  pages:number;
+  description: string;
+  website:string;
+  starRating: number;
+  imageUrl: string;
+}
